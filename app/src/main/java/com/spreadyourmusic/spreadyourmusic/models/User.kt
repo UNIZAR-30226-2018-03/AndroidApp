f64e5528ed259d505fa381ba3828d074242f192f
AbelChT <abelcht@outlook.es>
2018-04-02 15:28:47 +0200

    CDhange User

--- app/src/main/java/com/spreadyourmusic/spreadyourmusic/models/User.kt
+++ app/src/main/java/com/spreadyourmusic/spreadyourmusic/models/User.kt
@@ -1,7 +1,21 @@
 package com.spreadyourmusic.spreadyourmusic.models
 
+import android.graphics.Bitmap
+import java.util.*
+
 /**
  * Created by abel
  * On 7/03/18.
  */
-class User(val username: String, val firstName: String, val lastName: String, val pictureLocationUri: String) : Recommendation
+class User(val username: String, val name: String, val pictureLocationUri: String) : Recommendation {
+    var bibliography: String? = null
+    var email: String? = null
+    var password: String? = null
+    var birthDate: Date? = null
+    //String locale = context.getResources().getConfiguration().locale.getCountry();
+    var country: String? = null
+    var twitterAccount: String? = null
+    var facebookAccount: String? = null
+    var instagramAccount: String? = null
+    var picture:Bitmap? = null
+}
