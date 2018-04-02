69e54c84ab801ed1a0d54688eb16940d5cee04e9
AbelChT <abelcht@outlook.es>
2018-04-02 18:28:06 +0200

    Creacion pantalla de registro

--- app/src/main/java/com/spreadyourmusic/spreadyourmusic/activities/SignUpActivity.kt
+++ app/src/main/java/com/spreadyourmusic/spreadyourmusic/activities/SignUpActivity.kt
@@ -1,39 +1,56 @@
 package com.spreadyourmusic.spreadyourmusic.activities
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
 import com.spreadyourmusic.spreadyourmusic.R
 
 class SignUpActivity : AppCompatActivity() {
 
+    var numOfScreen = 1
+    val REQUEST_IMAGE_CAPTURE = 1067
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sign_up)
-
-        /*
-        *
-        *
-        * // Llamar a la camara
-        * static final int REQUEST_IMAGE_CAPTURE = 1;
-
-private void dispatchTakePictureIntent() {
-    Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-    if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        setContentView(R.layout.activity_sign_up_screen_1)
     }
-}
 
-
-
-@Override
-protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-    if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
-        Bundle extras = data.getExtras();
-        Bitmap imageBitmap = (Bitmap) extras.get("data");
-        mImageView.setImageBitmap(imageBitmap);
+    fun onContinueClick(v: View){
+        //TODO: Almacenar todos loas datos y hacer bien la transicion
+        //val inflator = layoutInflater
+        numOfScreen = 2
+        //val view = inflator.inflate(R.layout.activity_sign_up_screen_2, null, false)
+        //view.startAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right))
+        setContentView(R.layout.activity_sign_up_screen_2)
     }
-}
-        *
-        * */
+
+    fun onCreateClick(v:View){
+        //TODO: Almacenar todos loas datos
+    }
+
+    override fun onBackPressed() {
+       if(numOfScreen == 2){
+           //TODO: Hacer bien la transicion
+           numOfScreen = 1
+           setContentView(R.layout.activity_sign_up_screen_1)
+        }else {
+            super.onBackPressed()
+        }
+    }
+
+    fun onProfilePictureClick(v: View){
+        val takePictureIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (takePictureIntent.resolveActivity(packageManager) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE)
+        }
+    }
+
+    override fun onActivityResult(requestCode : Int,  resultCode : Int, data: Intent) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            val extras = data.extras
+            //val imageBitmap = extras.get("data")
+        }
     }
 }
