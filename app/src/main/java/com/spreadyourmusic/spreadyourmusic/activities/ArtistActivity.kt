b994d8aaf3bd72025456495084c414c9ebb02a59
AbelChT <abelcht@outlook.es>
2018-04-05 00:13:43 +0200

    errores compilacion

--- app/src/main/java/com/spreadyourmusic/spreadyourmusic/activities/ArtistActivity.kt
+++ app/src/main/java/com/spreadyourmusic/spreadyourmusic/activities/ArtistActivity.kt
@@ -2,9 +2,7 @@ package com.spreadyourmusic.spreadyourmusic.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import kotlinx.android.synthetic.main.activity_artist.*
 import com.spreadyourmusic.spreadyourmusic.R
-import android.R
 import android.support.v7.widget.*
 
 
@@ -16,24 +14,11 @@ class ArtistActivity : AppCompatActivity() {
         setContentView(R.layout.activity_artist)
 
         //App bar
-        val toolbar = findViewById(R.id.appbar) as Toolbar
+        val toolbar = findViewById(R.id.main_appbar) as Toolbar
         setSupportActionBar(toolbar)
         supportActionBar!!.setTitle("Mi Aplicación")
 
         //RecyclerView
-        val lstLista = findViewById(R.id.lstLista) as RecyclerView
-
-        val datos = ArrayList()
-        for (i in 0..49)
-            datos.add(Titular("Título $i", "Subtítulo item $i"))
-
-        val adaptador = AdaptadorTitulares(datos)
-        lstLista.adapter = adaptador
-
-        lstLista.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
-        lstLista.addItemDecoration(
-                DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST))
-        lstLista.itemAnimator = DefaultItemAnimator()
     }
 
 }
