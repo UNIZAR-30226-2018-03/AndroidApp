aafde5ca58f06aa9a3b357a870d625941b4fd2a3
yasmina97 <anaximandroabernathy@gmail.com>
2018-04-04 12:00:08 +0200

    Modificacion de pantalla de artistas y ArtistActivity. Falta acabar el Coordinatorlayout

--- app/src/main/java/com/spreadyourmusic/spreadyourmusic/activities/ArtistActivity.kt
+++ app/src/main/java/com/spreadyourmusic/spreadyourmusic/activities/ArtistActivity.kt
@@ -2,15 +2,38 @@ package com.spreadyourmusic.spreadyourmusic.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
-import com.spreadyourmusic.spreadyourmusic.R
 import kotlinx.android.synthetic.main.activity_artist.*
+import com.spreadyourmusic.spreadyourmusic.R
+import android.R
+import android.support.v7.widget.*
+
 
 class ArtistActivity : AppCompatActivity() {
+    private val lstLista: RecyclerView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_artist)
-        setSupportActionBar(toolbar)
 
+        //App bar
+        val toolbar = findViewById(R.id.appbar) as Toolbar
+        setSupportActionBar(toolbar)
+        supportActionBar!!.setTitle("Mi Aplicación")
+
+        //RecyclerView
+        val lstLista = findViewById(R.id.lstLista) as RecyclerView
+
+        val datos = ArrayList()
+        for (i in 0..49)
+            datos.add(Titular("Título $i", "Subtítulo item $i"))
+
+        val adaptador = AdaptadorTitulares(datos)
+        lstLista.adapter = adaptador
+
+        lstLista.layoutManager = LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false)
+        lstLista.addItemDecoration(
+                DividerItemDecoration(this, DividerItemDecoration.VERTICAL_LIST))
+        lstLista.itemAnimator = DefaultItemAnimator()
     }
+
 }
