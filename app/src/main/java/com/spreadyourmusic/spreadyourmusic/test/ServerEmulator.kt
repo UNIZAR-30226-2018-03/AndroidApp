07aef8a47fed174cc443bb003a02a50b53433cbf
AbelChT <pgabeladc@gmail.com>
2018-04-22 19:26:42 +0200

    Prueba

--- app/src/main/java/com/spreadyourmusic/spreadyourmusic/test/ServerEmulator.kt
+++ app/src/main/java/com/spreadyourmusic/spreadyourmusic/test/ServerEmulator.kt
@@ -23,6 +23,7 @@ object ServerEmulator {
         val autor1 = User("Media", "Media", "Right", "http://storage.googleapis.com/automotive-media/album_art.jpg")
         val autor2 = User("Silent", "Silent", "Partner", "http://storage.googleapis.com/automotive-media/album_art.jpg")
         val autor3 = User("abelcht", "Abel ChT", "Lion", "http://storage.googleapis.com/automotive-media/album_art.jpg")
+        val autor4 = User("Prueba", "PRU", "sdfdsfd", "http://storage.googleapis.com/automotive-media/album_art.jpg")
 
         val album1 = Album("Jazz", autor1, GregorianCalendar(2018, 3, 22), "http://storage.googleapis.com/automotive-media/album_art.jpg")
         val album2 = Album("Blues", autor2, GregorianCalendar(2017, 6, 27), "http://storage.googleapis.com/automotive-media/album_art_2.jpg")
