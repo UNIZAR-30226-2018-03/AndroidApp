8982f501b4dd7b057d8db22197f7a95d7febe2d3
AbelChT <abelcht@outlook.es>
2018-05-28 21:39:44 +0200

    Fix soundviewer error

--- app/src/main/java/com/spreadyourmusic/spreadyourmusic/activities/SignUpActivity.kt
+++ app/src/main/java/com/spreadyourmusic/spreadyourmusic/activities/SignUpActivity.kt
@@ -183,7 +183,7 @@ class SignUpActivity : AppCompatActivity() {
             password = view!!.passwordEditText.text.toString().trim()
             mail = view!!.mailEditText.text.toString().trim()
 
-            if (password.isNullOrEmpty() || username.isNullOrEmpty() || (!isEdition && userPictureLocationUri.isNullOrEmpty())) {
+            if (userPictureLocationUri.isNullOrEmpty() || username.isNullOrEmpty() || (!isEdition && password.isNullOrEmpty())) {
                 Toast.makeText(activity, R.string.fill_all_fields, Toast.LENGTH_SHORT).show()
                 Toast.makeText(activity, R.string.empty_fields_1, Toast.LENGTH_SHORT).show()
 
